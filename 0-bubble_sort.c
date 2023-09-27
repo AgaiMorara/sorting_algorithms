296d94915eb1c9057511ead30bc51aa4b5820518
AgaiMorara <corneliusmorara@gmail.com>
2023-09-27 04:26:43 +0300

    improved bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,22 +1,7 @@
 #include "sort.h"
-/**
- *swap: swaps postions of two types
- *@array: the list to swap
- *@pos1: first position
- *@pos2: second position
- *
- */
-
-void swap(int *array, int pos1, int pos2)
-{
-	int temp = array[pos1];
-	array[pos1] = array[pos2];
-	array[pos2] = temp;
-}
-
 
 /**
- *bubble_sort : sorts an array in numeric or lexographic order
+ *bubble_sort - sorts an array in numeric or lexographic order
  *@array: array to be sorted
  *@size: size of the array
  */
@@ -24,16 +9,21 @@ void swap(int *array, int pos1, int pos2)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
+	int flag;
 
-	for ( i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
+		flag = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(array, j, j + 1);
 				print_array(array, size);
+				flag = 1;
 			}
 		}
+		if (flag == 0)
+			break;
 	}
 }
