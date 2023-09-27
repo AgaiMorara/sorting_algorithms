29dc84f4d53c62f6d3226729093679f192cbcb35
AgaiMorara <corneliusmorara@gmail.com>
2023-09-27 04:31:06 +0300

    optimized bubble

--- sort.h
+++ sort.h
@@ -20,23 +20,6 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-
-/**
- *swap: swaps postions of two types
- *@array: the list to swap
- *@pos1: first position
- *@pos2: second position
- *
- */
-
-void swap(int *array, int pos1, int pos2)
-{
-	int temp = array[pos1];
-
-	array[pos1] = array[pos2];
-	array[pos2] = temp;
-}
-
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
